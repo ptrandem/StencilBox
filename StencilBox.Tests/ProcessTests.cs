53906e679b4eb6899e19c6b11602f635a3298f5b
Paul Trandem <ptrandem@nerdery.com>
2014-07-31 15:22:36 -0500

    adding tests/pkgs

---
+++ StencilBox.Tests/ProcessTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StencilBox.Tests
+{
+    [TestClass]
+    public class ProcessTests
+    {
+        public class TestModel
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public bool IsTrue { get; set; }
+        }
+
+        [TestMethod]
+        public void Stencil_handles_normal_brackets_correctly()
+        {
+            const string template = "[~Title][][~Description]][[~IsTrue]";
+            const string expectedOutput = "title[]desc][True";
+            var model = new TestModel { Title="title", Description = "desc", IsTrue = true};
+
+            var output = Stencil.Apply(template, model);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+    }
+}
